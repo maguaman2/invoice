a8bf6b3b0e09f9f632cf336861e4374e46094c05
Marco <maguaman2@sudamericano.edu.ec>
2024-11-17 14:35:31 -0500

    first commit

---
+++ src/main/kotlin/com/example/invoice_project/InvoiceProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.invoice_project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InvoiceProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<InvoiceProjectApplication>(*args)
+}
